9f5e173f747104fefe192b83bf9145c0889a2927
Jonas de Jesus Américo <jjsamerico@gmail.com>
2022-11-29 20:52:38 -0300

    Aula: Implementando QueryCourses

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,7 +5,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/jonasjesusamerico/full-cycle-graphql/graph/generated"
 	"github.com/jonasjesusamerico/full-cycle-graphql/graph/model"
@@ -56,7 +55,19 @@ func (r *queryResolver) Categories(ctx context.Context) (categoriesModel []*mode
 
 // Courses is the resolver for the courses field.
 func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
-	panic(fmt.Errorf("not implemented: Courses - courses"))
+	courses, err := r.CourseDB.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	var coursesModel []*model.Course
+	for _, course := range courses {
+		coursesModel = append(coursesModel, &model.Course{
+			ID:          course.ID,
+			Name:        course.Name,
+			Description: &course.Description,
+		})
+	}
+	return coursesModel, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
